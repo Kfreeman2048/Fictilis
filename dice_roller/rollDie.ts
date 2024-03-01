1d060ec4285c1ebcf4f417421c37f6401d713111
Kfreeman2048 <kfreeman2048@gmail.com>
2024-02-29 21:49:16 -0600

    added a delay to diceroll to ensure a random seed

--- dice_roller/rollDie.ts
+++ dice_roller/rollDie.ts
@@ -1,5 +1,13 @@
+function wait(ms: number) {
+    var start = Date.now(),
+        now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
+}
 
 export function dieRoller(max: number): number
 {
+    wait(2);
     return Math.ceil(Math.random() * max);
 }

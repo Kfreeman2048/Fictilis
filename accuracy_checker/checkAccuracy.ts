b38b96536f8bed68abf69b134fdfd282544b32f0
Kfreeman2048 <kfreeman2048@gmail.com>
2024-03-07 00:53:06 -0600

    Created alternate accuracy for if you exceed by 5

--- accuracy_checker/checkAccuracy.ts
+++ accuracy_checker/checkAccuracy.ts
@@ -10,3 +10,19 @@ export function accuracyCheck(roll: number, target: number, DaD: number): boolea
     }
     else return false;
 }
+
+export function accuracyBeatBy5(roll: number, target: number, DaD: number): number 
+{
+    if (roll < (target + DaD)) {
+        return 0;
+    }
+
+    if (roll - (target + DaD) >= 5) {
+        return 2;
+    }
+
+    if (roll >= (target + DaD)) {
+        return 1;
+    }
+    else return 0;
+}

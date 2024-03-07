f61cea8f881528d23e84351a7d9eb4072bc8534c
Kfreeman2048 <kfreeman2048@gmail.com>
2024-03-07 00:53:49 -0600

    Removed defaulting to 8 for target on accuracy, not needed

--- accuracy_checker/checkAccuracy.ts
+++ accuracy_checker/checkAccuracy.ts
@@ -1,10 +1,5 @@
 export function accuracyCheck(roll: number, target: number, DaD: number): boolean
 {
-    if (target === null || target === 0)
-    {
-        target = 8;
-    }
- 
     if (roll >= target + DaD) {
         return true;
     }

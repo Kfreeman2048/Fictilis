8fd8b1c8242309571e7e39ee2a1e1091e46275fc
Kfreeman2048 <kfreeman2048@gmail.com>
2024-03-12 02:56:03 -0500

    added icicle Spear

--- actions/attacks.ts
+++ actions/attacks.ts
@@ -4,11 +4,25 @@ import{
   
 import{
     accuracyCheck,
+    accuracyBeatBy5,
 } from '../accuracy_checker/checkAccuracy.ts';
 
 export function slam(DaD: number, wDamage: number): number {
     if (accuracyCheck(dieRoller(20), 8, DaD)) {
         return (wDamage * 2);
+    }
+    else {
+        return 0;
+    }
+}
+
+export function icicleSpear(DaD: number, wDamage: number): number {
+    const accuracyValue = accuracyBeatBy5(dieRoller(20), 8, DaD);
+    if (accuracyValue === 1) {
+        return (wDamage);
+    }
+    if (accuracyValue === 2) {
+        return (wDamage + 8)
     }
     else {
         return 0;

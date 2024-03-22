24a56085ea6d4d70a3205079ce65aabc6dbf7056
Kfreeman2048 <kfreeman2048@gmail.com>
2024-03-22 03:23:14 -0500

    followed instructions for tamagui config

---
+++ tamagui.config.ts
@@ -0,0 +1,13 @@
+import { config } from '@tamagui/config/v3'
+import { Text, View } from 'react-native'
+import { createTamagui } from '@tamagui/core' // or '@tamagui/core'
+
+const appConfig = createTamagui(config)
+
+export type AppConfig = typeof appConfig
+
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends AppConfig {}
+}
+
+export default appConfig

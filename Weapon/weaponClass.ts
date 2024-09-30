c994d9d65725f84b2acdf403b7f30e20af6fd60b
Kfreeman2048 <kfreeman2048@gmail.com>
2024-09-29 21:28:55 -0500

    created weapon class

---
+++ Weapon/weaponClass.ts
@@ -0,0 +1,8 @@
+class weaponClass {
+    constructor (
+        private weaponName: string,
+        private wDamage: number,
+        private basicAttacks: Array<string>,
+    ) {}
+};
+export default weaponClass;
